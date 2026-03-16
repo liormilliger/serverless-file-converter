29c269e921cbbff0671b41f55635b6a23dcbeb6e
Lior Milliger <liormilliger@gmail.com>
2026-03-16 21:21:01 +0200

    Changing strategy - new set

--- executables.sh
+++ executables.sh
@@ -1,3 +1,14 @@
-sam deploy --guided --template cf-infra-pipeline.yaml
+# Enable the EventBridge notification setting on your manually managed bucket via the CLI (or Console):
+aws s3api put-bucket-notification-configuration \
+    --bucket liorm-bronze-bucket \
+    --notification-configuration '{ "EventBridgeConfiguration": {} }'
 
-sam delete --stack-name liorm-data-pipeline
+# DEPLOY STACK
+aws cloudformation deploy \
+  --template-file template.yaml \
+  --stack-name data-digestion-3-tier \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --tags Name=3-tier-data-digestion User=liorm-at-polus
+
+# DELETE STACK
+aws cloudformation delete-stack --stack-name data-digestion-3-tier
